05fe192ae61055f0d29e8b450a83f5be02a5069b
Aleksey <altl01@bk.ru>
2023-09-16 00:53:48 +0700

    EX-2: create decorator

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,12 +1,11 @@
-from flask import jsonify, Response, request
+from flask import request
 from flask.json import dumps
 from webargs.flaskparser import use_args
 from apps.articles.enums import ArticleStatusEnum
 from apps.articles.managers import ArticleManager
 from apps.articles.models import Article
 from apps.articles.schemas import ArticleSchema, ArticlePublicationSchema, ArticleFilterSchema
-from apps.articles.utils import create_response
-from apps.core.extensions import db
+from apps.articles.utils import create_response, provide_session
 from flask import Blueprint
 
 
@@ -14,24 +13,26 @@ article_bp = Blueprint("article", __name__, url_prefix='/articles/')
 
 
 @article_bp.route("", methods=['POST'])
-def create():
+@provide_session
+def create(session):
     data = request.get_json()
     schema = ArticleSchema()
     cleaned_data = schema.load(data)
     article = Article(**cleaned_data)
-    db.session.add(article)
-    db.session.commit()
+    session.add(article)
+    session.commit()
     return create_response(schema.dumps(article), status_code=200)
 
 
 @article_bp.route("<int:pk>", methods=['PUT'])
-def update_article(pk=None):
-    article = db.session.query(Article).filter(Article.id == pk).first()
+@provide_session
+def update_article(session, pk=None):
+    article = session.query(Article).filter(Article.id == pk).first()
     data = request.get_json()
     schema = ArticleSchema()
     updated_data = schema.load(data)
-    db.session.query(Article).filter(Article.id == pk).update(updated_data)
-    db.session.commit()
+    session.query(Article).filter(Article.id == pk).update(updated_data)
+    session.commit()
     data = ArticleSchema().dump(article)
     return create_response(dumps(data), status_code=201)
 
@@ -55,28 +56,31 @@ def get_all_articles(args):
 
 
 @article_bp.route("/<int:id>", methods=['GET'])
-def get_id_articles(id):
-    article = db.session.query(Article).get_or_404(id)
+@provide_session
+def get_id_articles(session, id):
+    article = session.query(Article).get_or_404(id)
     serializer = ArticleSchema()
     data = serializer.dump(article)
     return create_response(dumps(data), status_code=200)
 
 
 @article_bp.route("<int:id>", methods=['DELETE'])
-def delete_article(id):
-    article = db.session.query(Article).get_or_404(id)
+@provide_session
+def delete_article(session, id):
+    article = session.query(Article).get_or_404(id)
     article.status = ArticleStatusEnum.DELETED.value
-    db.session.commit()
+    session.commit()
     return create_response(status_code=204)
 
 
 @article_bp.route("<int:id>/publication", methods=['POST'])
-def publish_article(id):
-    article = db.session.query(Article).get_or_404(id)
+@provide_session
+def publish_article(session, id):
+    article = session.query(Article).get_or_404(id)
     data = request.get_json()
     schema = ArticlePublicationSchema()
     cleaned_data = schema.load(data)
     article.status = ArticleStatusEnum.PUBLISHED.value if cleaned_data[
         'is_published'] else ArticleStatusEnum.DRAFT.value
-    db.session.commit()
+    session.commit()
     return create_response()
